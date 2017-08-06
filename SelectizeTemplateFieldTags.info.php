ded54e020460c2a4062c034bf8054002e3e87162
Marc <mw@ajcg.org>
2017-08-06 11:13:17 -0400

    Update for core selectize
    This version removes the requirement for the JquerySelectize module,
    when running in PW 3.0.67 or higher.

---
+++ SelectizeTemplateFieldTags.info.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$info = array(
+    'title'     => 'Selectize Template & Field Tags',
+    'author'    => 'Macrura',
+    'version'   => 004,
+    'summary'   => 'Admin helper for enabling selectize tagging on templates.',
+    'href'      => '',
+    'icon'      => 'tags',
+    'singular'  => true,
+    'autoload'  => true,
+    //'requires'  => array("JquerySelectize"),
+	);
+
+if($this->wire('config')->version < '3.0.67') {
+	$info['requires'] = "JquerySelectize";
+}
